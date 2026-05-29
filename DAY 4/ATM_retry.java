2b97d85631927bf5a88a2b83bde88aca834cf637
25BCSH95-Tanmaya-Kumar-Pradhan <ultraaeonus@gmail.com>
2026-05-29 15:40:13 +0530

    Add files via upload

---
+++ DAY 4/ATM_retry.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ATM_retry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int correct_pin=2451;
+		int pin;
+		do
+		{
+			System.out.println("Enter the pin (4-digit):");
+			pin = sc.nextInt();
+			if(pin!=correct_pin)
+			{
+				System.out.println("Wrong pin! Please try again!");
+			}
+			
+		}while(pin!=correct_pin);
+		
+		System.out.println("Successfully logged into account!");
+		sc.close();
+	}
+
+}
---
+++ DAY 4/Palindrome_checker.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Palindrome_checker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int num = sc.nextInt();
+		int temp=num;
+		int rev=0;
+		while(temp>0)
+		{
+			rev=(rev*10)+temp%10;
+			temp/=10;
+		}
+		if(rev==num)
+		{
+			System.out.println("Palindrome number!");
+		}
+		else
+		{
+			System.out.println("Not palindrome!");
+		}
+		sc.close();
+	}
+
+}
